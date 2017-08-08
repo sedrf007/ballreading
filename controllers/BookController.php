8dcaa4c371d028954bec29d85fc3e320a98642c8
jichenjie <jichenjie@zuihuibao.com>
2017-08-08 17:19:18 +0800

    js of booklist

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -54,17 +54,24 @@ class BookController extends Controller
         $userinfo = Reader::findOne(['username'=>$username]);
 
         $book = Books::findOne(['id'=>$id]);
+        $book_name = $book->book_name;
         $book->status = $status;
         $book->save();
 
         $email = $book->owner_email();
-        Yii::$app->mailer->compose()
+        $mail = Yii::$app->mailer->compose()
             //->setFrom('sedrf008@126.com')
             ->setTo($email)
             ->setSubject('AlphaYang图书馆')
-            ->setTextBody('<br>借了都要还的')
-            ->setHtmlBody('<br>借阅人'.$userinfo->username.
-                '</br><br>地址：'.$userinfo->address.'</br><br>借阅人电话：'.$userinfo->phone.'</br>')
-            ->send();
+            ->setTextBody('<br>借了都要还的');
+            if($status){
+                $mail->setHtmlBody('<br>借阅书籍：'.$book_name.'</br><br>借阅人:'.$userinfo->username.
+                    '</br><br>地址：'.$userinfo->address.'</br><br>借阅人电话：'.$userinfo->phone.'</br>');
+            }else{
+                $mail->setHtmlBody('<br>归还书籍：'.$book_name.'</br><br>归还人:'.$userinfo->username.
+                    '</br><br>地址：'.$userinfo->address.'</br><br>归还人电话：'.$userinfo->phone.'</br>');
+            }
+
+        $mail->send();
     }
 }
