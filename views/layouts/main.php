08663d030996d5df2a05c43badbf88691d7c8bca
jichenjie <jichenjie@zuihuibao.com>
2017-08-11 14:03:02 +0800

    master

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -56,12 +56,12 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
-<!--    <div class="container">-->
+    <div class="container">
 <!--        --><?//= Breadcrumbs::widget([
 //            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 //        ]) ?>
-<!--        --><?//= $content ?>
-<!--    </div>-->
+        <?= $content ?>
+    </div>
 </div>
 
 <footer class="footer">
