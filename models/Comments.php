887a71a792d0c2f9e86aa4beb626b11d1380e991
jichenjie <jichenjie@zuihuibao.com>
2018-01-12 17:55:44 +0800

    nov

---
+++ models/Comments.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2018/1/12
+ * Time: 17:16
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Comments extends ActiveRecord
+{
+
+}
