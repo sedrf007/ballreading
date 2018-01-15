c647896839ddd5654f128ed493c58d6e9b34ef2c
jichenjie <jichenjie@zuihuibao.com>
2018-01-15 16:45:23 +0800

    masterpiece

--- web/js/nov/article.js
+++ web/js/nov/article.js
@@ -23,21 +23,24 @@ function articledetail()
 function addcomment(id)
 {
     var comment = ($('#comment').val());
-    var p = {
-        id:id,
-        comment:comment
-    };
-    $.ajax({
-        type: 'post',
-        data: p,
-        contentType:"application/x-www-form-urlencoded;charset=UTF-8",
-        url: '/nov/add-comment',
-        dataType: 'json',
-        success: function()
-        {
-            layer.msg('评论成功！');
-            window.location.reload();
-        }
-    });
-
+    if(comment.length == 0){
+        layer.error('评论不能为空');
+    }else{
+        var p = {
+            id:id,
+            comment:comment
+        };
+        $.ajax({
+            type: 'post',
+            data: p,
+            contentType:"application/x-www-form-urlencoded;charset=UTF-8",
+            url: '/nov/add-comment',
+            dataType: 'json',
+            success: function()
+            {
+                layer.msg('评论成功！');
+                window.location.reload();
+            }
+        });
+    }
 }
