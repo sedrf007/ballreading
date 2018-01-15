458895319c05681dbc69c650df6608832719fa23
jichenjie <jichenjie@zuihuibao.com>
2018-01-15 14:07:00 +0800

    delete pingan

--- views/nov/articledetail.php
+++ views/nov/articledetail.php
@@ -35,6 +35,8 @@ $this->title = 'AlphaYang的图书馆';
     ?>
 <?php }
 ?>
+<textarea class="form-control" rows="3" placeholder="您的评论..."></textarea>
+<button class="btn btn-default" type="submit" style="float: right">Button</button>
 
 
 
